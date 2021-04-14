538f576019951c7004f4bb6173b123b1243bbad8
Tina Shan <tinashan22@gmail.com>
2021-04-14 11:51:14 +0800

    initializing /Slash MP

---
+++ app.js
@@ -0,0 +1,27 @@
+// app.js
+App({
+  onLaunch() {
+    wx.BaaS = requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment)
+
+    let clientID = 'a3fb2113c0a97830d15b'  // 应用名称: TeamSlash的第一个小程序
+    wx.BaaS.init(clientID)
+
+
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
